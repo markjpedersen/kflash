9e8b6fcecec609b9eb29e8cc1c2d462cfae9a36a
markjpedersen <markpedersen410@gmail.com>
2019-11-22 08:59:23 -0600

    Added other modified files that I should have a moment ago...

--- K210Isp.py
+++ K210Isp.py
@@ -10,100 +10,169 @@ from struct import Struct
 import binascii
 from enum import Enum
 
-class IspMessage:
+# The various "reasons" for a message, which is part of the header.
 
-    err = {0x00 : 'Default',
-           0xe0 : 'Ok',
-           0xe1 : 'BadDataLen',
-           0xe2 : 'BadCrc',
-           0xe3 : 'InvalidCmd'}
-    
-    Header = Struct('<HHI')     # op, zero, crc
+err = {'Default'    : 0x00,
+       'Ok'         : 0xe0,
+       'BadDataLen' : 0xe1,
+       'BadCrc'     : 0xe2,
+       'InvalidCmd' : 0xe3,
+       'InitFailed' : 0xe4}
 
+# Empty messages contain nothing but short headers.
+
+class EmptyMessage:
+
+    header = Struct('<BB')     # op, reason
     op = None
-    crc = 0
-    body = b''
+    reason = 0
     
-    def __init__(self, buf=b''):
-        self.body = buf
-        self.crc = binascii.crc32(self.body) & 0xffffffff
+    def __init__(self, reason=0):
+        self.reason = reason
         
     def print(self): print(self.__dict__)
     
     def format(self):
-        return self.Header.pack(self.op, 0, self.crc) + self.body
+        return self.header.pack(self.op, self.reason)
 
     @classmethod
     def parseHeader(cls, buf):
-        op, _, crc = cls.Header.unpack_from(buf)
-        rest = buf[cls.Header.size:]
-        return op, crc, rest
+        op, reason = cls.header.unpack_from(buf)
+        return op, reason
         
     @classmethod
     def parse(cls, buf):
-        op, crc, rest = cls.parseHeader(buf)
-        return cls(rest)
+        op, reason = cls.parseHeader(buf)
+        return cls(reason)
 
 
-class IspNop(IspMessage): op = 0xc2
-class IspEcho(IspNop): op = 0xc1
-class IspFlashNop(IspNop): 0xd2
+# A regular message is an empty message with a CRC and a variable
+# length body of binary data.
 
-class IspMemRead(IspMessage):
+class Message(EmptyMessage):
+
+    header = Struct('<HHI')     # op, reason, crc
+    crc = 0
+    body = b''
+    
+    def __init__(self, reason=0, buf=b''):
+        self.body = buf
+        self.crc = binascii.crc32(self.body) & 0xffffffff
+        super().__init__(self.reason)
+        
+    def print(self): print(self.__dict__)
+    
+    def format(self):
+        return self.header.pack(self.op, self.reason, self.crc) + self.body
+    
+    @classmethod
+    def parseHeader(cls, buf):
+        op, reason, crc = cls.header.unpack_from(buf)
+        rest = buf[cls.header.size:]
+        return op, reason, crc, rest
+        
+    @classmethod
+    def parse(cls, buf):
+        op, reason, crc, rest = cls.parseHeader(buf)
+        return cls(reason, rest)
+
+
+class Nop(EmptyMessage): op = 0xc2
+class FlashGreet(EmptyMessage): op = 0xd2
+class FlashErase(EmptyMessage): op = 0xd3
+class MemReadReply(EmptyMessage): op = 0xc4
+class MemWriteReply(EmptyMessage): op = 0xc3
+class FlashWriteReply(EmptyMessage): op = 0xd4
+class InitFlashReply(EmptyMessage): op = 0xd7
+
+class Echo(Message): op = 0xc1
+
+class MemRead(Message):
     op = 0xc4
     Body = Struct('<II')     # adrs, len
-    def __init__(self, adrs, len, dat):
+    def __init__(self, adrs, len, reason=0, dat=b''):
         self.adrs = adrs
         self.len = len
+        self.reason = reason
         self.dat = dat
-        super().__init__(self.Body.pack(self.adrs, self.len) + self.dat)
+        super().__init__(self.reason, self.Body.pack(self.adrs, self.len) + self.dat)
     @classmethod
     def parse(cls, buf):
-        op, crc, rest = cls.parseHeader(buf)
+        op, reason, crc, rest = cls.parseHeader(buf)
         adrs, len = cls.Body.unpack_from(rest)
-        return cls(adrs, len, rest[cls.Body.size:])
+        return cls(adrs, len, reason, rest[cls.Body.size:])
     
-class IspMemWrite(IspMemRead): op = 0xc3
+class MemWrite(MemRead): op = 0xc3
+class FlashWrite(MemRead): op = 0xd4
 
-class IspMemBoot(IspMessage):
+class MemBoot(Message):
     op = 0xc5
     Body = Struct('<II')        # adrs, len (zero)
-    def __init__(self, adrs):
+    def __init__(self, adrs, reason=0):
         self.adrs = adrs
-        super().__init__(self.Body.pack(self.adrs, 0x0))
+        self.reason = reason
+        super().__init__(self.reason, self.Body.pack(self.adrs, 0x0))
     @classmethod
     def parse(cls, buf):
-        op, crc, rest = cls.parseHeader(buf)
+        op, reason, crc, rest = cls.parseHeader(buf)
         adrs, len = cls.Body.unpack_from(rest)
-        return cls(adrs)
+        return cls(adrs, reason)
 
-class IspSetBaud(IspNop):
+class InitFlash(Message):
+    op = 0xd7
+    Body = Struct('<II')        # chiptype, 0
+    def __init__(self, chip, reason=0):
+        self.chip = chip
+        super().__init__(self.reason, self.Body.pack(self.chip, 0x0))
+    @classmethod
+    def parse(cls, buf):
+        op, reason, crc, rest = cls.parseHeader(buf)
+        chip, _ = cls.Body.unpack_from(rest)
+        return cls(chip, reason)
+    
+class SetBaud(Message):
     op = 0xd6
     Body = Struct('<III')       # 0, 4, baudrate
-    def __init__(self, rate):
+    def __init__(self, rate, reason=0):
+        self.reason = reason
         self.rate = rate
-        super().__init__(self.Body.pack(0, 4, self.rate))
+        super().__init__(self.reason, self.Body.pack(0, 4, self.rate))
     @classmethod
     def parse(cls, buf):
-        op, crc, rest = cls.parseHeader(buf)
+        op, reason, crc, rest = cls.parseHeader(buf)
         _, _, rate = cls.Body.unpack_from(rest)
-        return cls(rate)
+        return cls(rate, reason)
 
-class IspSetBaudStage0(IspSetBaud): op = 0xc6
-class IspDebugInfo(IspNop): op = 0xd1
+class SetBaudStage0(SetBaud): op = 0xc6
+class DebugInfo(Message): op = 0xd1
 
 
 
 # Parse a formatted message.
 
-def parseIspMessage(buf):
-    dispatch = {0xc1 : IspEcho,
-                0xc2 : IspNop,
-                0xc3 : IspMemWrite,
-                0xc4 : IspMemRead,
-                0xc5 : IspMemBoot,
-                0xd6 : IspSetBaud,
-                0xc6 : IspSetBaudStage0,
-                0xd1 : IspDebugInfo}
-    op, crc, rest = IspMessage.parseHeader(buf)
-    return dispatch[op].parse(buf)
+def parseMessage(buf):
+    if len(buf) == 2:
+        dispatch = {0xc2 : Nop,
+                    0xc3 : MemWriteReply,
+                    0xc4 : MemReadReply,
+                    0xd2 : FlashGreet,
+                    0xd3 : FlashErase,
+                    0xd4 : FlashWriteReply,
+                    0xd7 : InitFlashReply}
+        op, reason = EmptyMessage.parseHeader(buf)
+        return dispatch[op].parse(buf)
+    else:
+        dispatch = {0xc1 : Echo,
+                    0xc2 : Nop,
+                    0xc3 : MemWrite,
+                    0xc4 : MemRead,
+                    0xc5 : MemBoot,
+                    0xd6 : SetBaud,
+                    0xc6 : SetBaudStage0,
+                    0xd1 : DebugInfo,
+                    0xd2 : FlashGreet,
+                    0xd3 : FlashErase,
+                    0xd4 : FlashWrite,
+                    0xd7 : InitFlash}
+        op, reason, crc, rest = Message.parseHeader(buf)
+        return dispatch[op].parse(buf)
--- poke.py
+++ poke.py
@@ -6,149 +6,37 @@
 # Dan board.
 #
 
+import gc
 import time
 import serial
 import binascii
-from enum import Enum
-import struct
-
-
-def log(*args, **kwargs):
-    print(*args, **kwargs)
+import K210Isp as Isp
+from MaixLoader import MAIXLoader
     
+def readSram():
+    print('Read SRAM.')
+    l.write(Isp.MemRead(0x80000005, 10, 0, b'').format())
+    Isp.parseMessage(l.read()).print()
 
-class ISP:
+def initFlash():
+    print('Greet Flash.')
+    l.write(Isp.FlashGreet().format())
+    Isp.parseMessage(l.read()).print()
+    print('Init Flash.')
+    l.write(Isp.InitFlash(1).format())
+    Isp.parseMessage(l.read()).print()
 
-    class Operation(Enum):
-        ISP_ECHO = 0xC1
-        ISP_NOP = 0xC2
-        ISP_MEMORY_WRITE = 0xC3
-        ISP_MEMORY_READ = 0xC4
-        ISP_MEMORY_BOOT = 0xC5
-        ISP_DEBUG_INFO = 0xD1
-        ISP_CHANGE_BAUDRATE = 0xc6
-
-    class Error(Enum):
-        ISP_RET_DEFAULT = 0
-        ISP_RET_OK = 0xE0
-        ISP_RET_BAD_DATA_LEN = 0xE1
-        ISP_RET_BAD_DATA_CHECKSUM = 0xE2
-        ISP_RET_INVALID_COMMAND = 0xE3
-
-    cmd = struct.Struct('<HHIII')
-    DebugCmd = struct.pack('<HH', 0xd1, 0)
+def eraseFlash():
+    initFlash()
+    print('Erase Flash.')
+    l.write(Isp.FlashErase().format())
+    Isp.parseMessage(l.read()).print()
     
-    @staticmethod
-    def parse(data):
-
-        # type: (bytes) -> (int, int, str)
-
-        op = int(data[0])
-        reason = int(data[1])
-        text = ''
-
-        try:
-            if ISP.Operation(op) == ISP.Operation.ISP_DEBUG_INFO:
-                text = data[2:].decode()
-        except ValueError:
-            KFlash.log('Warning: recv unknown op', op)
-
-        return (op, reason, text)
-
-class MAIXLoader:
-
-    def __init__(self, port='/dev/ttyUSB1', baudrate=115200):
-
-        # configure the serial connections (the parameters differs on
-        # the device you are connecting to)
-
-        self._port = serial.Serial(
-            port=port,
-            baudrate=baudrate,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=1)
-        
-        log("Default baudrate is", baudrate)
-
-    def reset_to_isp_dan(self):
-
-        # Pull RESET and IO_16 down and keep 10ms.
-        
-        self._port.setDTR (True)
-        self._port.setRTS (True)
-        time.sleep(0.01)
-
-        # Release RESET, leaving IO_16 low to enter ISP mode.
-        
-        self._port.setDTR (True)
-        self._port.setRTS (False)
-        time.sleep(0.01)
-
-    def reset_to_boot_dan(self):
-
-        # Pull RESET down and keep 10ms.
-        
-        self._port.setDTR (False)
-        self._port.setRTS (True)
-        time.sleep(0.01)
-
-        # Release RESET, leaving IO_16 high to boot from flash.
-        
-        self._port.setDTR (False)
-        self._port.setRTS (False)
-        time.sleep(0.01)
-
-    def write(self, packet):
-        buf = b'\xc0' \
-              + (packet.replace(b'\xdb', b'\xdb\xdd').replace(b'\xc0', b'\xdb\xdc')) \
-              + b'\xc0'
-        log('[WRITE]', binascii.hexlify(buf))
-        return self._port.write(buf)
-
-    def read(self):
-
-        # First, listen for a packet start token, \xc0.  It needs to
-        # arrive within a serial read timeout.  If it doesn't, or we get
-        # anything else, throw an exception.
-
-        buf = self._port.read(1)
-        if buf != b'\xc0':
-            raise Exception("SLIP read wrong start token: ", buf[0])
-
-        # Listen until we get a full packet, terminated by \xc0.
-
-        buf += self._port.read_until('\xff', self._port.in_waiting)
-        log("read", len(buf), "bytes: ", str(buf))
-
-        # Now translate any SLIP escape sequences and ditch the
-        # start and stop tokens.
-
-        packet = buf[1:-1].replace(b'\xdb\xdc', b'\xc0').replace(b'\xdb\xdd', b'\xdb')
-        return packet
-
-    def poke(self):
-
-        # buf = ISP.cmd.pack(0xc2, 0, 0, 0, 0)
-        adrs = 0x80000000
-        n = 10
-        crc = binascii.crc32(struct.pack('<II', adrs, n)) & 0xffffffff
-        log('crc: ', hex(crc))
-        buf = ISP.cmd.pack(0xc4, 0, crc, adrs, n)
-        log('xmit buf: ', str(buf))
-        self.write(buf)
-        time.sleep(1)
-        reply = self.read()
-        log('MAIX returned: ', str(reply))
-        op, reason, text = ISP.parse(reply)
-        log('ISP response:', ISP.Operation(op).name,
-            ISP.Error(reason).name, text)
-
-
 l = MAIXLoader('/dev/ttyUSB0', baudrate=115200)
 
-l.reset_to_isp_dan()
-l.poke()
-l.reset_to_boot_dan()
+l.reset_to_isp()
+
+initFlash()
+
+l.reset_to_boot()
 
